e395db2c962a9a15980377bfb55892d470b2e1f3
rodoggx <mikerodolph@yahoo.com>
2016-08-02 16:32:29 -0400

    add complex JSONObject

--- app/src/main/java/com/example/jsonparse/MainActivity.java
+++ app/src/main/java/com/example/jsonparse/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivityTAG_";
     private String jsonString = "[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]";
+    private String jsonComplexString = "{\"students\":[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +39,24 @@ public class MainActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    public void doComplexMagic(View view) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonComplexString);
+            JSONArray jsonArray = new JSONArray(jsonObject.getJSONArray("students").toString());
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject student = new JSONObject(jsonArray.get(i).toString());
+
+                String name = student.getString("name");
+                String grade = student.getString("grade");
+                String age = student.getString("age");
+                Log.d(TAG, "doComplexMagic: " + i + " " + name + " " + grade + " " + age);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
